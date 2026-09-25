0b6d16fe28652cec4cd02447072404b22592bad5
tchoyieric-hash <tchoyieric@gmail.com>
2026-09-25 02:42:39 +0100

    Create multer.ts

---
+++ ​config/multer.ts
@@ -0,0 +1,13 @@
+import multer from 'multer';
+
+export const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5 Mo max
+  fileFilter: (_req, file, cb) => {
+    const allowed = ['image/jpeg', 'image/png', 'image/webp'];
+    if (!allowed.includes(file.mimetype)) {
+      return cb(new Error('Format de fichier non autorisé (jpeg, png, webp uniquement)'));
+    }r
+    cb(null, true);
+  },
+});
